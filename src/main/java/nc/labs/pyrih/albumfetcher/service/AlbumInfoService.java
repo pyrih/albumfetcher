a55f8245d280747fd25722ecf3571e6d3463c892
Andrii Pyrih <andrii.pyrih@gmail.com>
2020-03-03 22:29:58 +0200

    Added service classes to service package.

---
+++ src/main/java/nc/labs/pyrih/albumfetcher/service/AlbumInfoService.java
@@ -0,0 +1,9 @@
+package nc.labs.pyrih.albumfetcher.service;
+
+import nc.labs.pyrih.albumfetcher.model.Album;
+
+public interface AlbumInfoService {
+    Album getAlbum();
+
+    Album getAlbumDoc();
+}
---
+++ src/main/java/nc/labs/pyrih/albumfetcher/service/AlbumInfoServiceImpl.java
@@ -0,0 +1,15 @@
+package nc.labs.pyrih.albumfetcher.service;
+
+import nc.labs.pyrih.albumfetcher.model.Album;
+
+public class AlbumInfoServiceImpl implements AlbumInfoService {
+    @Override
+    public Album getAlbum() {
+        return null;
+    }
+
+    @Override
+    public Album getAlbumDoc() {
+        return null;
+    }
+}
