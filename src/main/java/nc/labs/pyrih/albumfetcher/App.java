92bd4c597f344dacfa4149383b4fdfe07aa0fc7b
Andrii Pyrih <andrii.pyrih@gmail.com>
2020-04-10 11:55:17 +0300

    Deleted AsyncConfig.java (used TaskExecutorBuilder to create TaskExecutor) and add @EnableAsync to App.java, added Executor configuration to application.properties

--- src/main/java/nc/labs/pyrih/albumfetcher/App.java
+++ src/main/java/nc/labs/pyrih/albumfetcher/App.java
@@ -5,12 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.client.RestTemplate;
 
 import java.time.Duration;
 
 @SpringBootApplication
 @EnableCaching
+@EnableAsync
 public class App {
 
     private static final long SECONDS = 1000L;
